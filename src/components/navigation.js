1bd4f3a9bcf0bf3e886132599bd8a0f3a5dc50d4
shuupsy <101631301+shuupsy@users.noreply.github.com>
2022-06-22 12:12:14 +0200

    Evenements tartine

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './navigation.css'
 
 class Navigation extends React.Component {
+    navigation = (destination) => { console.log(destination)}
     render() {
         return (
             <nav>
-                <button>Link 1</button>
-                <button>Link 2</button>
+                <button onClick={() => this.navigation("Accueil")}>Link 1</button>
+                <button onMouseOver={() => this.navigation("Galerie")}>Link 2</button>
                 <button>Link 3</button>
             </nav>
         )
