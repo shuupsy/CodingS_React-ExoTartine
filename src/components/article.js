4580c4540b05bbdb45cea6e28c5b04733e58f41a
shuupsy <101631301+shuupsy@users.noreply.github.com>
2022-06-23 11:22:46 +0200

    Tartine, exo props

--- src/components/article.js
+++ src/components/article.js
@@ -3,12 +3,13 @@ import tartine from './tartine.png'
 import './article.css';
 
 class Article extends React.Component {
+    Props = () => console.log(this.props);
     render() {
         return (
             <div class="article">
                 <img src={tartine} alt="tartine"/>
                 <div class="texte">
-                    <button>Quel est le titre</button>
+                    <button onClick={this.Props}>Quel est le titre</button>
                     <h2>Mon premier article</h2>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic quae culpa voluptate dolorum facilis repellendus natus harum quam pariatur dolor quo maxime voluptates nesciunt tenetur similique, eaque rerum quod ad deleniti quisquam odio officiis. In voluptates, aliquid voluptate laborum earum laudantium consequuntur perspiciatis veritatis quis minima fugiat, a totam reiciendis!</p>
                 </div>
--- src/components/main.js
+++ src/components/main.js
@@ -2,20 +2,18 @@ import React from 'react';
 import './main.css'
 import Article from './article.js';
 
-class Main extends React.Component {
-    nombreArticle = 2;
-    render() {
+function Main (props){
+    let nombreArticle = 2;
         // let nombreArticle = 2;
-        if(this.nombreArticle >= 2) {
-            return (
-                <main>
-                    <Article/>
-                    <Article/>
-                </main>
-            )
-        } else {
-            return(<h1>Il n'y a pas d'article.</h1>)
-        }
+    if(nombreArticle >= 2) {            
+        return (
+            <main>
+                <Article titre="Les tartines"/>
+                <Article titre="Coding School"/>
+            </main>
+        )
+    } else {
+        return(<h1>Il n'y a pas d'article.</h1>)
     }
 }
 
