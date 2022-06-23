86f88d3c84d42e3e46aec01a507183544c4b9d32
shuupsy <101631301+shuupsy@users.noreply.github.com>
2022-06-23 14:10:26 +0200

    Exercice: props enfant-parent

--- src/components/article.js
+++ src/components/article.js
@@ -4,12 +4,14 @@ import './article.css';
 
 class Article extends React.Component {
     Props = () => console.log(this.props);
+    nuage = 2;
     render() {
         return (
             <div class="article">
                 <img src={tartine} alt="tartine"/>
                 <div class="texte">
                     <button onClick={this.Props}>Quel est le titre</button>
+                    <button onClick={() => this.props.ciel(this.nuage)}>COUCOU CEST MOI</button>
                     <h2>Mon premier article</h2>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic quae culpa voluptate dolorum facilis repellendus natus harum quam pariatur dolor quo maxime voluptates nesciunt tenetur similique, eaque rerum quod ad deleniti quisquam odio officiis. In voluptates, aliquid voluptate laborum earum laudantium consequuntur perspiciatis veritatis quis minima fugiat, a totam reiciendis!</p>
                 </div>
--- src/components/main.js
+++ src/components/main.js
@@ -3,12 +3,13 @@ import './main.css'
 import Article from './article.js';
 
 function Main (props){
+    let soleil = (x) => console.log(x)
+
     let nombreArticle = 2;
-        // let nombreArticle = 2;
     if(nombreArticle >= 2) {            
         return (
             <main>
-                <Article titre="Les tartines"/>
+                <Article titre="Les tartines" ciel = {(x) => soleil(x)}/>
                 <Article titre="Coding School"/>
             </main>
         )
